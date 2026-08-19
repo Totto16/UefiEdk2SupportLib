ec80ba0e02e157059099356434477f402da0eb10
Totto16 <tobiausgais@gmail.com>
2026-08-19 23:41:25 +0200

    feat: add some things to the default clock lib

--- Library/Default/clock.c
+++ Library/Default/clock.c
@@ -3,77 +3,14 @@
 #include <errno.h>
 
 #include <Base.h>
-#include <IndustryStandard/Acpi.h>
 #include <Library/BaseLib.h>
 #include <Library/DebugLib.h>
-#include <Library/IoLib.h>
-#include <Library/PcdLib.h>
-#include <Library/PciLib.h>
 #include <Uefi.h>
 
+#include <Library/PcdLib.h>
+#include <Register/Cpuid.h>
 
-#include <Library/HobLib.h>
-#include <Library/TimerLib.h>
-#include <PiDxe.h>
-
-#include <Library/BaseMemoryLib.h>
-
-static UINT64 mAcpiTimerLibTscFrequency = 0;
-
-
-//TODO: this doesn't really work, use another implementation
-
-// parts are from: PcAtChipsetPkg/Library/AcpiTimerLib/AcpiTimerLib.c
-
-GUID mFrequencyHobGuid = {
-    0x3fca54f6,
-    0xe1a2,
-    0x4b20,
-    { 0xbe, 0x76, 0x92, 0x6b, 0x4b, 0x48, 0xbf, 0xaa }
-};
-
-
-/**
-  Internal function to retrieves the 64-bit frequency in Hz.
-
-  Internal function to retrieves the 64-bit frequency in Hz.
-
-  @return The frequency in Hz.
-
-**/
-UINT64
-InternalGetPerformanceCounterFrequency(VOID);
-
-
-/**
-  Internal function to retrieve the ACPI I/O Port Base Address.
-
-  Internal function to retrieve the ACPI I/O Port Base Address.
-
-  @return The 16-bit ACPI I/O Port Base Address.
-
-**/
-UINT16
-InternalAcpiGetAcpiTimerIoPort(VOID) {
-    UINT16 Port;
-
-    Port = PcdGet16(PcdAcpiIoPortBaseAddress);
-
-    //
-    // If the register offset to the BAR for the ACPI I/O Port Base Address is not 0x0000, then
-    // read the PCI register for the ACPI BAR value in case the BAR has been programmed to a
-    // value other than PcdAcpiIoPortBaseAddress
-    //
-    if (PcdGet16(PcdAcpiIoPciBarRegisterOffset) != 0x0000) {
-        Port = PciRead16(PCI_LIB_ADDRESS(
-                PcdGet8(PcdAcpiIoPciBusNumber), PcdGet8(PcdAcpiIoPciDeviceNumber), PcdGet8(PcdAcpiIoPciFunctionNumber),
-                PcdGet16(PcdAcpiIoPciBarRegisterOffset)
-        ));
-    }
-
-    return (Port & PcdGet16(PcdAcpiIoPortBaseAddressMask)) + PcdGet16(PcdAcpiPm1TmrOffset);
-}
-
+// some code is based on: UefiCpuPkg/Library/CpuTimerLib/CpuTimerLib.c
 
 /**
   Retrieves the current value of a 64-bit free running performance counter.
@@ -94,124 +31,19 @@ GetPerformanceCounterImpl(VOID) {
 }
 
 
-/**
-  The constructor function enables ACPI IO space.
+static UINT64 mAcpiTimerLibTscFrequency = 0;
 
-  If ACPI I/O space not enabled, this function will enable it.
-  It will always return RETURN_SUCCESS.
-
-  @retval EFI_SUCCESS   The constructor always returns RETURN_SUCCESS.
-
-**/
-RETURN_STATUS
-EFIAPI
-AcpiTimerLibConstructor(VOID) {
-    UINTN Bus;
-    UINTN Device;
-    UINTN Function;
-    UINTN EnableRegister;
-    UINT8 EnableMask;
-
-    //
-    // ASSERT for the invalid PCD values. They must be configured to the real value.
-    //
-    ASSERT(PcdGet16(PcdAcpiIoPciBarRegisterOffset) != 0xFFFF);
-    ASSERT(PcdGet16(PcdAcpiIoPortBaseAddress) != 0xFFFF);
-
-    //
-    // If the register offset to the BAR for the ACPI I/O Port Base Address is 0x0000, then
-    // no PCI register programming is required to enable access to the ACPI registers
-    // specified by PcdAcpiIoPortBaseAddress
-    //
-    if (PcdGet16(PcdAcpiIoPciBarRegisterOffset) == 0x0000) {
-        return RETURN_SUCCESS;
-    }
-
-    //
-    // ASSERT for the invalid PCD values. They must be configured to the real value.
-    //
-    ASSERT(PcdGet8(PcdAcpiIoPciDeviceNumber) != 0xFF);
-    ASSERT(PcdGet8(PcdAcpiIoPciFunctionNumber) != 0xFF);
-    ASSERT(PcdGet16(PcdAcpiIoPciEnableRegisterOffset) != 0xFFFF);
-
-    //
-    // Retrieve the PCD values for the PCI configuration space required to program the ACPI I/O Port Base Address
-    //
-    Bus = PcdGet8(PcdAcpiIoPciBusNumber);
-    Device = PcdGet8(PcdAcpiIoPciDeviceNumber);
-    Function = PcdGet8(PcdAcpiIoPciFunctionNumber);
-    EnableRegister = PcdGet16(PcdAcpiIoPciEnableRegisterOffset);
-    EnableMask = PcdGet8(PcdAcpiIoBarEnableMask);
-
-    //
-    // If ACPI I/O space is not enabled yet, program ACPI I/O base address and enable it.
-    //
-    if ((PciRead8(PCI_LIB_ADDRESS(Bus, Device, Function, EnableRegister)) & EnableMask) != EnableMask) {
-        PciWrite16(
-                PCI_LIB_ADDRESS(Bus, Device, Function, PcdGet16(PcdAcpiIoPciBarRegisterOffset)),
-                PcdGet16(PcdAcpiIoPortBaseAddress)
-        );
-        PciOr8(PCI_LIB_ADDRESS(Bus, Device, Function, EnableRegister), EnableMask);
-    }
-
-    return RETURN_SUCCESS;
-}
 
 /**
-  Calculate TSC frequency.
+  Internal function to retrieves the 64-bit frequency in Hz.
 
-  The TSC counting frequency is determined by comparing how far it counts
-  during a 101.4 us period as determined by the ACPI timer.
-  The ACPI timer is used because it counts at a known frequency.
-  The TSC is sampled, followed by waiting 363 counts of the ACPI timer,
-  or 101.4 us. The TSC is then sampled again. The difference multiplied by
-  9861 is the TSC frequency. There will be a small error because of the
-  overhead of reading the ACPI timer. An attempt is made to determine and
-  compensate for this error.
+  Internal function to retrieves the 64-bit frequency in Hz.
 
-  @return The number of TSC counts per second.
+  @return The frequency in Hz.
 
 **/
-UINT64
-InternalCalculateTscFrequency(VOID) {
-    UINT64 StartTSC;
-    UINT64 EndTSC;
-    UINT16 TimerAddr;
-    UINT32 Ticks;
-    UINT64 TscFrequency;
-    BOOLEAN InterruptState;
-
-    InterruptState = SaveAndDisableInterrupts();
-
-    TimerAddr = InternalAcpiGetAcpiTimerIoPort();
-    //
-    // Compute the number of ticks to wait to measure TSC frequency.
-    // Use 363 * 9861 = 3579543 Hz which is within 2 Hz of ACPI_TIMER_FREQUENCY.
-    // 363 counts is a calibration time of 101.4 uS.
-    //
-    Ticks = IoBitFieldRead32(TimerAddr, 0, 23) + 363;
-
-    StartTSC = AsmReadTsc(); // Get base value for the TSC
-    //
-    // Wait until the ACPI timer has counted 101.4 us.
-    // Timer wrap-arounds are handled correctly by this function.
-    // When the current ACPI timer value is greater than 'Ticks',
-    // the while loop will exit.
-    //
-    while (((Ticks - IoBitFieldRead32(TimerAddr, 0, 23)) & BIT23) == 0) {
-        CpuPause();
-    }
-
-    EndTSC = AsmReadTsc(); // TSC value 101.4 us later
-
-    TscFrequency = MultU64x32(
-            (EndTSC - StartTSC), // Number of TSC counts in 101.4 us
-            9861                 // Number of 101.4 us in a second
-    );
-
-    SetInterruptState(InterruptState);
-
-    return TscFrequency;
+UINT64 InternalGetPerformanceCounterFrequency(VOID) {
+    return mAcpiTimerLibTscFrequency;
 }
 
 
@@ -310,17 +142,65 @@ int clock_getres(clockid_t clockid, struct timespec* res) {
 
 
 /**
-  The constructor function enables ACPI IO space.
+  CPUID Leaf 0x15 for Core Crystal Clock Frequency.
 
-  If ACPI I/O space not enabled, this function will enable it.
-  It will always return RETURN_SUCCESS.
-
-  @retval EFI_SUCCESS   The constructor always returns RETURN_SUCCESS.
+  The TSC counting frequency is determined by using CPUID leaf 0x15. Frequency in MHz = Core XTAL frequency * EBX/EAX.
+  In newer flavors of the CPU, core xtal frequency is returned in ECX or 0 if not supported.
+  @return The number of TSC counts per second.
 
 **/
-RETURN_STATUS
-EFIAPI
-AcpiTimerLibConstructor(VOID);
+static UINT64 CpuidCoreClockCalculateTscFrequency(OUT EFI_STATUS* Status) {
+    UINT64 TscFrequency;
+    UINT64 CoreXtalFrequency;
+    UINT32 RegEax;
+    UINT32 RegEbx;
+    UINT32 RegEcx;
+
+    //
+    // Use CPUID leaf 0x15 Time Stamp Counter and Nominal Core Crystal Clock Information
+    // EBX returns 0 if not supported. ECX, if non zero, provides Core Xtal Frequency in hertz.
+    // TSC frequency = (ECX, Core Xtal Frequency) * EBX/EAX.
+    //
+    AsmCpuid(CPUID_TIME_STAMP_COUNTER, &RegEax, &RegEbx, &RegEcx, NULL);
+
+    //
+    // If EAX or EBX returns 0, the XTAL ratio is not enumerated.
+    //
+    if ((RegEax == 0) || (RegEbx == 0)) {
+        if (RegEax == 0) {
+            *Status = EFI_UNSUPPORTED;
+            return 0;
+        }
+
+        if (RegEbx == 0) {
+            *Status = EFI_UNSUPPORTED;
+            return 0;
+        }
+
+        *Status = EFI_SUCCESS;
+        return 0;
+    }
+
+    //
+    // If ECX returns 0, the XTAL frequency is not enumerated.
+    // And PcdCpuCoreCrystalClockFrequency defined should base on processor series.
+    //
+    if (RegEcx == 0) {
+        CoreXtalFrequency = PcdGet64(PcdCpuCoreCrystalClockFrequency);
+    } else {
+        CoreXtalFrequency = (UINT64) RegEcx;
+    }
+
+    //
+    // Calculate TSC frequency = (ECX, Core Xtal Frequency) * EBX/EAX
+    //
+    TscFrequency = DivU64x32(MultU64x32(CoreXtalFrequency, RegEbx) + (UINT64) (RegEax >> 1), RegEax);
+
+    *Status = EFI_SUCCESS;
+    return TscFrequency;
+}
+
+//from: PcAtChipsetPkg/Library/AcpiTimerLib/AcpiTimerLib.c
 
 /**
   Calculate TSC frequency.
@@ -337,62 +217,61 @@ AcpiTimerLibConstructor(VOID);
   @return The number of TSC counts per second.
 
 **/
+/*
 UINT64
-InternalCalculateTscFrequency(VOID);
+InternalCalculateTscFrequency(VOID) {
+    UINT64 StartTSC;
+    UINT64 EndTSC;
+    UINT16 TimerAddr;
+    UINT32 Ticks;
+    UINT64 TscFrequency;
+    BOOLEAN InterruptState;
 
-/**
-  Internal function to retrieves the 64-bit frequency in Hz.
+    InterruptState = SaveAndDisableInterrupts();
 
-  Internal function to retrieves the 64-bit frequency in Hz.
-
-  @return The frequency in Hz.
-
-**/
-UINT64
-InternalGetPerformanceCounterFrequency(VOID) {
-    return mAcpiTimerLibTscFrequency;
-}
-
-
-/**
-  The constructor function enables ACPI IO space, and caches PerformanceCounterFrequency.
-
-  @retval EFI_SUCCESS   The constructor always returns RETURN_SUCCESS.
-
-**/
-EFI_STATUS
-CommonAcpiTimerLibConstructor(VOID) {
-    EFI_HOB_GUID_TYPE* GuidHob;
-
+    TimerAddr = InternalAcpiGetAcpiTimerIoPort();
     //
-    // Enable ACPI IO space.
+    // Compute the number of ticks to wait to measure TSC frequency.
+    // Use 363 * 9861 = 3579543 Hz which is within 2 Hz of ACPI_TIMER_FREQUENCY.
+    // 363 counts is a calibration time of 101.4 uS.
     //
-    AcpiTimerLibConstructor();
+    Ticks = IoBitFieldRead32(TimerAddr, 0, 23) + 363;
 
+    StartTSC = AsmReadTsc(); // Get base value for the TSC
     //
-    // Initialize PerformanceCounterFrequency
+    // Wait until the ACPI timer has counted 101.4 us.
+    // Timer wrap-arounds are handled correctly by this function.
+    // When the current ACPI timer value is greater than 'Ticks',
+    // the while loop will exit.
     //
-    GuidHob = GetFirstGuidHob(&mFrequencyHobGuid);
-    if (GuidHob != NULL) {
-        mAcpiTimerLibTscFrequency = *(UINT64*) GET_GUID_HOB_DATA(GuidHob);
-    } else {
-        mAcpiTimerLibTscFrequency = InternalCalculateTscFrequency();
+    while (((Ticks - IoBitFieldRead32(TimerAddr, 0, 23)) & BIT23) == 0) {
+        CpuPause();
     }
 
-    return EFI_SUCCESS;
+    EndTSC = AsmReadTsc(); // TSC value 101.4 us later
+
+    TscFrequency = MultU64x32(
+            (EndTSC - StartTSC), // Number of TSC counts in 101.4 us
+            9861                 // Number of 101.4 us in a second
+    );
+
+    SetInterruptState(InterruptState);
+
+    return TscFrequency;
 }
+    */
 
-/**
-  The constructor function enables ACPI IO space, and caches PerformanceCounterFrequency.
 
-  @param  ImageHandle   The firmware allocated handle for the EFI image.
-  @param  SystemTable   A pointer to the EFI System Table.
-
-  @retval EFI_SUCCESS   The constructor always returns RETURN_SUCCESS.
-
-**/
 EFI_STATUS
 EFIAPI
 OOpetrisSupportLibConstructorSupportClockDefault(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable) {
-    return CommonAcpiTimerLibConstructor();
+
+    EFI_STATUS Status = EFI_SUCCESS;
+    mAcpiTimerLibTscFrequency = CpuidCoreClockCalculateTscFrequency(&Status);
+
+    // if (EFI_ERROR(Status)) {
+    //     mAcpiTimerLibTscFrequency = InternalCalculateTscFrequency();
+    // }
+
+    return Status;
 }
